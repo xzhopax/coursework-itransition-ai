b2ea9292a72e5ac4264c3f00d696a199a8649eee
DampCave <xzhopax@gmail.com>
2022-02-21 01:00:50 +0300

    Initial commit

---
+++ src/main/java/com/dampcave/courseworkitransitionai/CourseworkItransitionAiApplication.java
@@ -0,0 +1,13 @@
+package com.dampcave.courseworkitransitionai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseworkItransitionAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseworkItransitionAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dampcave/courseworkitransitionai/CourseworkItransitionAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dampcave.courseworkitransitionai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseworkItransitionAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
